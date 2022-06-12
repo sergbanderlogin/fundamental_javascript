0bff7b203b50ed7a1745cd392aba7c5d8e7d64b6
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-12 17:03:36 +0300

    create validtion form

---
+++ 07_authentication/07_02_validation_forms/src/js/views/form.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @param {String} msg
+ */
+
+function inputErrorTemplate(msg) {
+  return `
+    <div class="invalid-feedback">${msg}</div>
+  `;
+}
+
+/**
+ * Function showInputError. Add input error
+ * @param {HTMLInputElement} el
+ */
+
+export function showInputError(el) {
+  const paretn = el.parentElement;
+  const msg = el.dataset.invalidMessage || "Invalid input";
+  const template = inputErrorTemplate(msg);
+  el.classList.add("is-invalid");
+  paretn.insertAdjacentHTML("beforeend", template);
+}
+
+export function removeInputError(el) {
+  const parent = el.parentElement;
+  const err = parent.querySelector(".invalid-feedback");
+  if (!err) return;
+
+  el.classList.remove("is-invalid");
+  parent.removeChild(err);
+}
