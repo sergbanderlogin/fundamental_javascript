57ef9bc7bedce0505ce8eaf40d8ee3dabaaf440f
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-13 12:02:26 +0300

    organized a request to the server

---
+++ 07_authentication/07_03_request_server/src/js/app.js
@@ -0,0 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/style.css";
+
+import UI from "./config/ui.config";
+import { validate } from "./helpers/validate";
+import { showInputError, removeInputError } from "./views/form";
+import { login } from "./services/auth.service";
+
+const { form, inputEmail, inputPassword } = UI;
+const inputs = [inputEmail, inputPassword];
+
+// Events
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  onSubmit();
+});
+
+inputs.forEach((el) =>
+  el.addEventListener("focus", () => removeInputError(el))
+);
+
+// Handlers
+async function onSubmit() {
+  const isValidForm = inputs.every((el) => {
+    const isValidInput = validate(el);
+    if (!isValidInput) {
+      showInputError(el);
+    }
+
+    return isValidInput;
+  });
+
+  if (!isValidForm) return;
+
+  try {
+    await login(inputEmail.value, inputPassword.value);
+    form.reset();
+    // show seccess notify
+  } catch (err) {
+    // show error notify
+  }
+}
