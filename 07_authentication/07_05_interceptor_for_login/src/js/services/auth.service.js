4ee01d3c1ae5b0a516ea13e3337289e9daee0336
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-13 20:59:39 +0300

    added interceptor for login

---
+++ 07_authentication/07_05_interceptor_for_login/src/js/services/auth.service.js
@@ -0,0 +1,22 @@
+import axios from "../plugins/axios";
+
+/**
+ *
+ * @param {String} email
+ * @param {String} password
+ */
+
+export async function login(email, password) {
+  try {
+    const response = await axios.post(
+      `/auth/login`,
+      JSON.stringify({ email, password }),
+    );
+
+    console.log(response);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+    return Promise.reject(err);
+  }
+}
