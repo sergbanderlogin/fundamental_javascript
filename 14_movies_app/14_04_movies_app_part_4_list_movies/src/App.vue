8f16403490d46c1c135fe4edeefa9896bc6f8016
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-23 00:31:17 +0300

    add list movies

---
+++ 14_movies_app/14_04_movies_app_part_4_list_movies/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <MoviesList :list="moviesList" />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
+
+export default {
+  name: "app",
+  components: {
+    MoviesList,
+  },
+  computed: {
+    ...mapGetters("movies", ["moviesList"]),
+  },
+  methods: {
+    ...mapActions("movies", ["fetchMovies"]),
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
