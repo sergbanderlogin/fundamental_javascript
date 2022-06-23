4640965a0bde60db5454a869455295b0264ee5d8
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-23 12:04:45 +0300

    added background poster

---
+++ 14_movies_app/14_05_movies_app_part_5_poster/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <PosterBG :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
+import PosterBG from "@/components/PosterBG";
+
+export default {
+  name: "app",
+  components: {
+    MoviesList,
+    PosterBG,
+  },
+  data: () => ({
+    posterBg: "",
+  }),
+  computed: {
+    ...mapGetters("movies", ["moviesList"]),
+  },
+  methods: {
+    ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+}
+</style>
---
+++ 14_movies_app/14_05_movies_app_part_5_poster/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import movies from "./modules/movies";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    movies,
+  },
+});
+
+store.dispatch("initMoviesStore");
+
+export default store;
