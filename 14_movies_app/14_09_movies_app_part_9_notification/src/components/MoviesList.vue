b933385f00ed08b59c19489dfa5211bf5fcf4271
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-24 22:45:42 +0300

    added notification

---
+++ 14_movies_app/14_09_movies_app_part_9_notification/src/components/MoviesList.vue
@@ -0,0 +1,76 @@
+<template>
+  <BContainer>
+    <h3 class="list-title">{{ listTitle }}</h3>
+    <BRow>
+      <template v-if="isExist">
+        <BCol cols="3" v-for="(movie, key) in list" :key="key">
+          <MovieItem
+            :movie="movie"
+            @mouseover.native="onMouseOver(movie.Poster)"
+            @removeItem="onRemoveItem"
+          />
+        </BCol>
+      </template>
+      <template v-else>
+        <div>Empty List</div>
+      </template>
+    </BRow>
+  </BContainer>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MovieItem from "./MovieItem";
+
+export default {
+  name: "MoviesList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  components: {
+    MovieItem,
+  },
+  computed: {
+    ...mapGetters("movies", ["isSearch"]),
+    isExist() {
+      return Boolean(Object.keys(this.list).length);
+    },
+    listTitle() {
+      return this.isSearch ? "Search result" : "IMDB Top 250";
+    },
+  },
+  methods: {
+    ...mapActions("movies", ["removeMovie"]),
+    ...mapActions(["showNotify"]),
+    onMouseOver(poster) {
+      this.$emit("changePoster", poster);
+    },
+    async onRemoveItem({ id, title }) {
+      const isComfirmed = await this.$bvModal.msgBoxConfirm(
+        `Are you sure delete ${title}?`
+      );
+      // console.log(isComfirmed);
+
+      if (isComfirmed) {
+        this.removeMovie(id);
+        this.showNotify({
+          msg: "Movie deleted successful",
+          variant: "success",
+          title: "Success",
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.list-title {
+  font-size: 50px;
+  margin-bottom: 30px;
+  color: #fff;
+}
+</style>
