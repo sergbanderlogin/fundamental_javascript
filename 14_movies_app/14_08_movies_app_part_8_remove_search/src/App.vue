c7b0b2a037339ee6e79aec5fe7d0b7dcbe0ad57a
sergbanderlogin <sergbanderlogin@gmail.com>
2022-06-24 13:24:53 +0300

    added remove and search movies

---
+++ 14_movies_app/14_08_movies_app_part_8_remove_search/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <Loader />
+    <PosterBG :poster="posterBg" />
+    <Header />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination
+      :current-page="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
+import PosterBG from "@/components/PosterBG";
+import MoviesPagination from "@/components/MoviesPagination";
+import Loader from "@/components/Loader";
+import Header from "@/components/Header";
+
+export default {
+  name: "app",
+  components: {
+    MoviesList,
+    PosterBG,
+    MoviesPagination,
+    Loader,
+    Header,
+  },
+  data: () => ({
+    posterBg: "",
+  }),
+  computed: {
+    ...mapGetters("movies", [
+      "moviesList",
+      "currentPage",
+      "moviesPerPage",
+      "moviesLength",
+    ]),
+  },
+  watch: {
+    "$route.query": {
+      handler: "onPageQueryChange",
+      immediate: true,
+      deep: true,
+    },
+  },
+  methods: {
+    ...mapActions("movies", ["changeCurrentPage"]),
+    onPageQueryChange({ page = 1 }) {
+      this.changeCurrentPage(Number(page));
+    },
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.$router.push({ query: { page } });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+}
+</style>
---
+++ 14_movies_app/14_08_movies_app_part_8_remove_search/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import movies from "./modules/movies";
+import loader from "./modules/loader";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    movies,
+    loader,
+  },
+});
+
+store.dispatch("initMoviesStore");
+
+export default store;
